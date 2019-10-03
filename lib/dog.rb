29a8aa5828c0ed80e2b139581d5a7bf42e1d26a8
Pablo Gonzalez <pabgon0@gmail.com>
2019-10-03 20:10:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ require "pry"
 class Dog
   attr_accessor :id, :name, :breed, :hash
 
-  def initialize(hash, id = nil)
+  def initialize(hash)
     @id = hash[0]
     @name = hash[1]
     @breed = hash[2]
