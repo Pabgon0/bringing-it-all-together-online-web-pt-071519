a5deb07bab1731c905ac60f92be67555471d8966
Pablo Gonzalez <pabgon0@gmail.com>
2019-10-03 20:27:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,8 +51,10 @@ class Dog
     Dog.new(id: id, name: name, breed: breed)
   end
 
-  def self.find_by_id
-    
+  def self.find_by_id(id)
+    sql = "SELECT * FROM songs WHERE id = ?"
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
   end
 
 
